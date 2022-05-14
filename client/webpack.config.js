6f71add4648f900c001ff7254a625be3177f1f0c
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-05-13 22:23:57 -0400

    add jate name to webpack config & GET jate db js logic complete

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "JATE",
       }),
 
       // Injects our custom service worker
@@ -31,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "pwa-text-editor",
+        short_name: "JATE",
+        description: "Text Editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
